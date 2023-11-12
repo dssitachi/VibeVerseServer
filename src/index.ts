d9d3854546299917ce36a5cd150ae04bf8e149e4
Dipesh Shivrame <dipeshshivrame@gmail.com>
2023-11-13 00:16:29 +0530

    express setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello world")
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import compression from 'compression';
+
+const app = express();
+
+app.use(cors({
+    credentials: true
+}))
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('Server started on port 8080');
+})
